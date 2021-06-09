8cfb9319962610f35febf677c63fcfbe07064d27
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2021-06-10 06:11:16 +0800

    refactor: check constraint stack method

--- main_script.py
+++ main_script.py
@@ -1,25 +1,57 @@
+import re
+from typing import ChainMap
 from header_script import *
 
 def in_degree(graph, vertex):
     return len(graph.in_edges(vertex))
 
 def check_constraints(graph, source, sink, flow):
-    # Constraint 1: flow <= capacity
-    for f in flow:
-        flow_cost = flow[f]
-        tail = f[0]
+    current_vertex = source
+    to_visit = [current_vertex]
 
-        # Access capacity cost
-        for g in graph.out_edges(tail):
-            print(flow_cost, g.capacity)
-            if (g.tail,g.head) == f and flow_cost > g.capacity:
+    while to_visit:
+        outward = graph.out_edges(current_vertex)
+        inward = graph.in_edges(current_vertex)
+        total_out_flow = 0
+        total_in_flow = 0
+        
+        for out in outward:
+            flow_cost = flow[(current_vertex, out.head)]
+            capacity = out.capacity
 
+            # Constraint 1: flow <= capacity
+            if flow_cost > capacity:
+                return False
+            total_out_flow += flow_cost
+
+            # Remove redundant visit
+            if out.head not in to_visit:
+                to_visit.append(out.head)
+
+        # Constraint 2: flow in = flow out
+        total_in_flow = 0
+        if current_vertex != source and current_vertex != sink:
+            # Determine flow going into vertex
+            total_in_flow = sum([flow[(inw.tail, current_vertex)] for inw in inward])
+
+            if total_in_flow != total_out_flow:
                 return False
 
-    print()
+            # Constraint 3: flow splits evenly between edges
+            expected_flow_split_cost = total_in_flow/len(outward)
+            for out in outward:
+                flow_cost = flow[(current_vertex, out.head)]
+                if not is_equal(flow_cost, expected_flow_split_cost):
+                    return False
+
+        if to_visit:
+            to_visit.pop(0)
+            if to_visit:
+                current_vertex = to_visit[0]
+
     return True
-    # Constraint 2: flow in = flow out
-    # Constraint 3: flow splits evenly between edges
+
+    
 
 def is_feasible(graph, source, sink, flow_value):
     pass
